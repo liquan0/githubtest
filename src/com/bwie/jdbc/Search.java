d839e80b4429a07e909db0784105c51c08bf7588
liquan0 <flyaiping@yeah.net>
2018-05-10 12:58:26 +0800

    添加的信息

--- src/com/bwie/jdbc/Search.java
+++ src/com/bwie/jdbc/Search.java
@@ -3,6 +3,11 @@ package com.bwie.jdbc;
 public class Search {
     public static void main(String[] args) {
 		 System.out.println("打印信息");
+		  System.out.println("测试信息");
+		    
+		 
+		 
+		 
 	}
 	
 	
